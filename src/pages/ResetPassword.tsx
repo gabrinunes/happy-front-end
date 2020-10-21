57b4d17c26b75da28e9da78e3de3fe751507c96c
Gabriel Cunha <gn_cunha@hotmail.com>
2020-10-21 18:20:29 -0300

    ajustes na página de redefinição de senha

--- src/pages/ResetPassword.tsx
+++ src/pages/ResetPassword.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../styles/pages/forgot-password.css';
 import logoImg from '../images/Logotipo.svg'
 import { Link } from 'react-router-dom';
-import {FiArrowLeft} from 'react-icons/fi'
+import {FiArrowLeft,FiEye,FiEyeOff} from 'react-icons/fi'
 
 
 export default function ForgotPassword(){
+
+      const [showEye,SetShowEye] = useState(true);
+      const [showEyeConfirmation,SetShowEyeConfirmation] = useState(true);
+      const [type,SetType] = useState('password')
+      const [typeConfirmation,SetTypeConfirmation] = useState('password')
+
+     function showPasswordField(event:any){
+         SetShowEye(false);
+         SetType('text')
+         console.log(event.type);
+     }
+
+     function NotshowPasswordField(){
+      SetShowEye(true);
+      SetType('password');
+      console.log(showEye);
+  }
+
+  function showPasswordConfirmationField(event:any){
+    SetShowEyeConfirmation(false);
+    SetTypeConfirmation('text')
+    console.log(event.type);
+}
+
+  function NotshowPasswordConfirmationField(){
+   SetShowEyeConfirmation(true);
+   SetTypeConfirmation('password');
+   console.log(showEye);
+  }
+
     return(
         <div id='page-reset'>
          
@@ -24,10 +54,20 @@ export default function ForgotPassword(){
                  acessar o dashboard do Happy</p>
 
                <label>Nova Senha</label>
-               <input type="text"/>
+               {showEye ? (
+                 <FiEyeOff size={16} color="#15C3D6" onClick={showPasswordField} className="eye"/>
+               ):(
+                 <FiEye size={16} color="#15c3d6" onClick={NotshowPasswordField} className="eye"/>
+               )}
+               <input type={type}/>
 
                <label>Repetir Senha</label>
-               <input type="text"/>
+               {showEyeConfirmation ? (
+                 <FiEyeOff size={16} color="#15C3D6" onClick={showPasswordConfirmationField} className="eye-repeat"/>
+               ):(
+                 <FiEye size={16} color="#15c3d6" onClick={NotshowPasswordConfirmationField} className="eye-repeat"/>
+               )}
+               <input type={typeConfirmation}/>
 
                <Link to="/forgotPassword" className="icon-back">
                  <FiArrowLeft size={22} color="#15C3D6"/>
